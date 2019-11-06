fecda13acc080d2cf0508e41fa351f72af1c6c89
brittanymae01 <brittanymae01>
2019-11-06 13:07:51 -0800

    created component files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import ApodGrid from './components/ApodGrid'
 
 function App() {
   return (
     <div className="App">
       <p>
+        <ApodGrid />
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
---
+++ src/components/ApodGrid.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ApodGrid() {
+    return (
+        <div>
+            <p>Hello</p>
+        </div>
+    )
+}
+
+export default ApodGrid
