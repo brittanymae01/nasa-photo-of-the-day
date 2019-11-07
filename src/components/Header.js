a2fd7b9a03b2ee1d7590e9b0c188a43f1c738482
brittanymae01 <brittanymae01>
2019-11-07 13:01:56 -0800

    added component

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <h1>Nasa Photo of the Day <span role="img" aria-label='rocketship emoji'>🚀</span>!</h1>
+    )
+}
+
+export default Header
